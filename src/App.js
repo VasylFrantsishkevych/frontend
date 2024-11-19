f456c977d94845bf6d90ce225b7f14d066b55857
VF <vasul.fr@gmail.com>
2024-11-20 00:01:23 +0200

    docker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get('/api/users').then(({data})=>setUsers(data))
+  }, []);
+    return (
+        <div>
+          <h1>Users:</h1>
+          {users.map(user=><div key={user._id}>{JSON.stringify(user)}</div> )}
+        </div>
+    );
+};
 
-export default App;
+export {App};
